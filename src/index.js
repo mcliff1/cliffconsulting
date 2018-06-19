5081acf124399a34fcd7a410331d846cfbcf5371
Matt Cliff <matt@denvercliffs.com>
2018-06-19 15:51:11 -0600

    import for service work

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-/* discussion of adding to framework -
- * https://github.com/facebook/create-react-app/issues/2398
- */
-// also see http://bit.ly/2vJdu84
 registerServiceWorker()
